811a03debdb619caf68f209a57ab079bc5c4ce83
janakaud <janakaud@gmail.com>
2014-02-25 22:00:49 +0530

    2/25/2014 10.00PM
    New classes added
    Get nearest waypoint method implemented

--- src/Server/Trip.java
+++ src/Server/Trip.java
@@ -6,6 +6,8 @@
 package Server;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  *
@@ -13,76 +15,100 @@ import java.util.ArrayList;
  */
 public class Trip {
 
-    int routeID;
-    int[] routeStationIdList;
-    String[] estimatedArrivalTimeForStations;
-    String[] estimatedArrivalTime;
-    boolean[] passedStationIds;
+	int routeID;
+	int[] routeStationIdList;
+	String[] estimatedArrivalTimeForStations;
+	String[] estimatedArrivalTime;
+	boolean[] passedStationIds;
+//
+	float startedLatitude;
+	float startedLongitude;
+	int passedCheckingFactor;
+	int nearestLocationId;
+//
+//	float[][] waypoints;
+	int[][] IdPreviousNext;
+//
+	private ArrayList<Waypoint> waypoints;
+	private Map<Integer, Station> stations;
+	//making singleton
+	public static ArrayList<Trip> trip = new ArrayList<>();
 
-    float startedLatitude;
-    float startedLongitude;
-    int passedCheckingFactor;
-    int nearestLocationId;
+	private Trip(int routeId) {
+		routeID = routeId;
+		setTripData();
+		estimatedArrivalTimeForStations();
+	}
 
-    float[][] wayPoints;
-    int[][] IdPreviousNext;
+	public static Trip getInstance(int routeID) {
+		for (int i = 0; i < trip.size(); i++) {
+			if (trip.get(i).routeID == routeID) {
+				return trip.get(i);
+			}
+		}
+		Trip newTrip = new Trip(routeID);
+		trip.add(newTrip);
 
-    //making singleton
-    public static ArrayList<Trip> trip = new ArrayList<>();
+		return newTrip;
+	}
 
-    private Trip(int routeId) {
-        routeID = routeId;
-        setTripData();
-        estimatedArrivalTimeForStations();
-    }
+	//////////////////////
+	public Waypoint getNearestWaypoint(int latitude, int longitude) {
+		float lon, lat, distance, min = Float.MAX_VALUE;
+		Waypoint temp = null, nearest = null;
+		Coordinate location = null;
 
-    public static Trip getInstance(int routeID) {
-        for (int i = 0; i < trip.size(); i++) {
-            if (trip.get(i).routeID == routeID) {
-                return trip.get(i);
-            }
-        }
-        Trip newTrip = new Trip(routeID);
-        trip.add(newTrip);
+		//compare with all waypoint positions
+		Iterator<Waypoint> iter = waypoints.iterator();
+		while(iter.hasNext()) {
+			temp = iter.next();
+			location = temp.getLocation();
+			lon = location.getLongitude();
+			lat = location.getLatitude();
 
-        return newTrip;
-    }
+			//calcuate (square of) distance
+			distance = (lon - longitude) * (lon - longitude)
+					+ (lat - latitude) * (lat - latitude);
+			//shortest distance?
+			if (distance < min) {
+				min = distance;
+				nearest = temp;
+			}
+		}
+		//returns null if no waypoints were found
+		return nearest;
+	}
 
-    //////////////////////
-    public int getIdOfNearestLocation(int latitude, int longitude) {
-        //get ID from the array
-        return 0;
-    }
+	public float[] getCordinatesOfTheStations(int stationID, int latitude, int longitude) {
+		return null;
+	}
 
-    public float[] getCordinatesOfTheStations(int stationID, int latitude, int longitude) {
-        return null;
-    }
+	public void countDelayToStations() {
+		
+	}
 
-    public void countDelayToStations() {
-    }
+	public void didStationPassed() {
+	}
 
-    public void didStationPassed() {
-    }
+	public void estimateTimeForAllStations() {
+	}
 
-    public void estimateTimeForAllStations() {
-    }
+	public void getTimeOfNeareestLocation() {
+	}
 
-    public void getTimeOfNeareestLocation() {
-    }
+	public void updateRasberryHandler() {
+		RasberryHandler.getInstance().updateFromTrip(routeID, routeStationIdList, passedStationIds, estimatedArrivalTime);
+	}
 
-    public void updateRasberryHandler() {
-        RasberryHandler.getInstance().updateFromTrip(routeID, routeStationIdList, passedStationIds, estimatedArrivalTime);
-    }
+	private void setTripData() {
+		//initialize routeStationIds
+		//initialize waypoints[][]
+		//initialize IdpreviousNext
+	}
 
-    private void setTripData() {
-        //initialize routeStationIds
-        //initialize waypoints[][]
-        //initialize IdpreviousNext
-    }
+	private void estimatedArrivalTimeForStations() {
+	}
 
-    private void estimatedArrivalTimeForStations() {
-    }
-
-    public void execute(LocationBox locationBox) {
-    }
+	public void execute(LocationBox locationBox) {
+	}
 }
